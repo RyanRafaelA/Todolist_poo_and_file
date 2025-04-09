c65db267bc4f26acea0cfc9cdfe2390661d836f0
RyanRafaelA <ryrafael461@gmail.com>
2025-04-08 22:11:03 -0300

    enum Status improvement

--- src/model/Status.java
+++ src/model/Status.java
@@ -1,7 +1,29 @@
 package model;
 
 public enum Status {
-	NAO_INICIADO,
-	EM_DESENVOLVIMENTO,
-	FINALIZADO;
+	NAO_INICIADO("NAO_INICIADO"),
+	EM_DESENVOLVIMENTO("EM_DESENVOLVIMENTO"),
+	FINALIZADO("FINALIZADO");
+	
+	String nameStatus;
+	
+	Status(String nameStatus){
+		this.nameStatus = nameStatus;
+	}
+	
+	public String getNameStatus() {
+		return nameStatus;
+	}
+	
+	
+	public static Status fromString(String status) {
+		if(status != null) {
+			for(Status s: Status.values()){
+				if(s.nameStatus.equalsIgnoreCase(status)) {
+					return s;
+				}
+			}
+		}
+		throw new IllegalArgumentException("Status invalido. "+status);
+	}
 }
